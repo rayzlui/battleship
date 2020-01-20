586d3b117e3bf0d1a5794881d742e92ec06442e9
Ray <rayzlui@gmail.com>
2020-01-20 13:26:03 -0800

    Updates

---
+++ precache-manifest.e7e77d34d27a3b55010ac3ce326ed372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ed451027694d1517287b018d3df2c8",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "93bd9b0a851643747501",
+    "url": "/battleship/static/css/main.ce089926.chunk.css"
+  },
+  {
+    "revision": "582961d02e327501f322",
+    "url": "/battleship/static/js/2.24011747.chunk.js"
+  },
+  {
+    "revision": "93bd9b0a851643747501",
+    "url": "/battleship/static/js/main.972a4b4b.chunk.js"
+  },
+  {
+    "revision": "c8c12a64d41e1b219556",
+    "url": "/battleship/static/js/runtime~main.26ba8606.js"
+  }
+]);
